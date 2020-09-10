77ee8dc60e3c7645a4ace2da4336d03a44a13d13
Claudio Lopez <claudioslopez04@gmail.com>
2020-09-10 19:40:57 -0300

    Updates

---
+++ precache-manifest.c8f5da7bd4b17bca49dd5ca6588c46e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cba9d0e579b552fd6e1620e8c77f89e",
+    "url": "/blackindigo/index.html"
+  },
+  {
+    "revision": "a17962a0b6ac85cb288d",
+    "url": "/blackindigo/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5851e8663b70edc07d29",
+    "url": "/blackindigo/static/js/2.9f5bb4f0.chunk.js"
+  },
+  {
+    "revision": "a17962a0b6ac85cb288d",
+    "url": "/blackindigo/static/js/main.af274494.chunk.js"
+  },
+  {
+    "revision": "a2ac1a5b6dee4a137dd1",
+    "url": "/blackindigo/static/js/runtime-main.891df8a6.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/blackindigo/static/media/logo.25bf045c.svg"
+  }
+]);
